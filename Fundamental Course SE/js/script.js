2e5068ca126e61988d019b33b58df7c3c7addab1
isnan101 <135939625+isnan101@users.noreply.github.com>
2023-06-08 16:36:26 +0700

    Add files via upload

---
+++ Fundamental Course SE/js/script.js
@@ -0,0 +1,15 @@
+// toggle class avtive
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#humberger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const humberger = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!humberger.contains(e.target) & !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
